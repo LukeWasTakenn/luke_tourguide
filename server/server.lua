cab10b40e47f757a4e2fdec7bd2647c3c6f30d5d
IDKThatGuy <63104021+IDKThatGuy@users.noreply.github.com>
2021-03-04 16:43:15 +0000

    Update server.lua

--- server/server.lua
+++ server/server.lua
@@ -8,7 +8,7 @@ AddEventHandler('luke_tourguide:Paycheck', function(numberOfStops)
     local xPlayer = ESX.GetPlayerFromId(src)
     local totalAmount = numberOfStops * Config.MoneyPerStop
 
-    if xPlayer ~= nil then
+    if xPlayer.job.name == 'tourguide' then
         if Config.PaymentInBank == false then
             xPlayer.addAccountMoney('bank', totalAmount)
             TriggerClientEvent('esx:showAdvancedNotification', src, 'Vinewood Tours', '', 'You completed the touring route, you recieved $~g~'..totalAmount, 'CHAR_MULTIPLAYER', 9)
